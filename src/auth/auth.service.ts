8282bd66949da31afceb58a280c818dd2e3fc5eb
mariojuretic <mario@weblio.hr>
2024-07-04 20:42:35 +0200

    add signup functionality, handle unique email error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import * as argon from 'argon2';
 
+import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
-    return {
-      data: dto,
-    };
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: AuthDto) {
+    // generate the password hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // save the new user in the db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      // return the saved user
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+
+      throw error;
+    }
   }
 }
